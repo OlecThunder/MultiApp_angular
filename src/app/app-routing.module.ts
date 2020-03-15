405bc17e0679aa20ae9a289730e30468d7bc4d2a
unknown <crazy.frog17052000@gmail.com>
2020-03-15 13:07:12 +0200

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { AuthComponent } from './components/auth/auth.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [{ path: '/auth', component: AuthComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
